0da703c51f34fe57a7ce80c51f10a96fe5e872ab
JaredMel <jareddmelendez1764@gmail.com>
2024-09-23 22:11:10 -0700

    WIP

---
+++ main.cpp
@@ -0,0 +1,30 @@
+///******************************************************************************
+//Description: This program will 
+//Author: Jared Melendez
+//COMSC 210 Section 5293
+//Date September 23, 2024
+//Status: WIP
+///******************************************************************************
+
+#include <iostream>
+using namespace std;
+
+class Color 
+{
+    private:
+        int red, green, blue;
+    public:
+        int getRed() {return red;}
+        int getGreen() {return green;}
+        int getBlue() {return blue;}
+        void setRed(int r) {red = r;}
+        void setGreen(int g) {green = g;}
+        void setBlue(int b) {blue = b;}
+};
+//Prototype Function
+
+//the main function
+int main()
+{
+    int n;
+}
